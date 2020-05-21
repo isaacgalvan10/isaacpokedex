fabe83de22f43ac77097d2be281e34173d461f3c
isaacgalvan10 <galvan1522@gmail.com>
2020-05-21 10:41:43 -0500

    adding inital files

---
+++ README.txt.txt
@@ -0,0 +1,2 @@
+This is my first API project.
+I will be using the Pokedex API to create a Pokedex with HTML CSS and Javascript.
---
+++ script.js
@@ -0,0 +1,19 @@
+
+const fetchPokemon = () => {
+  const url = `https://pokeapi.co/api/v2/pokemon/1`;
+  fetch(url)
+    .then( res => {
+      return res.json()
+    })
+    .then( data => {
+      console.log(data);
+      const pokemon = {};
+      pokemon['name'] = data.name;
+      pokemon['id'] = data.id;
+      pokemon['image'] = data.sprites['front_default']
+      pokemon['type'] = data.types.map( type => type.type.name);
+      console.log(pokemon);
+    })
+}
+
+fetchPokemon();
